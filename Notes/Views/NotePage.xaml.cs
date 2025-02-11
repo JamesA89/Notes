75642e859e0852c1ef773082d2df178ad77b7adb
jimbo8927 <40646650@live.napier.ac.uk>
2025-02-11 09:03:42 +0000

    Week 2 done, added models, made th eapp be able to save multiple notes and list them

--- Notes/Views/NotePage.xaml.cs
+++ Notes/Views/NotePage.xaml.cs
@@ -1,33 +1,57 @@
 namespace Notes.Views;
 
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
+
 public partial class NotePage : ContentPage
 {
 
-	string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
 	public NotePage()
+{
+    InitializeComponent();
+
+    string appDataPath = FileSystem.AppDataDirectory;
+    string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+    LoadNote(Path.Combine(appDataPath, randomFileName));
+}
+
+	private void LoadNote(string fileName)
+{
+    Models.Note noteModel = new Models.Note();
+    noteModel.Filename = fileName;
+
+    if (File.Exists(fileName))
+    {
+        noteModel.Date = File.GetCreationTime(fileName);
+        noteModel.Text = File.ReadAllText(fileName);
+    }
+
+    BindingContext = noteModel;
+}
+
+	private async void SaveButton_Clicked(object sender, EventArgs e)
+{
+    if (BindingContext is Models.Note note)
+        File.WriteAllText(note.Filename, TextEditor.Text);
+
+    await Shell.Current.GoToAsync("..");
+}
+
+private async void DeleteButton_Clicked(object sender, EventArgs e)
+{
+    if (BindingContext is Models.Note note)
+    {
+        // Delete the file.
+        if (File.Exists(note.Filename))
+            File.Delete(note.Filename);
+    }
+
+    await Shell.Current.GoToAsync("..");
+}
+
+	public string ItemId
 	{
-		InitializeComponent();
-
-
-		if (File.Exists(_fileName))
-        TextEditor.Text = File.ReadAllText(_fileName);
+    	set { LoadNote(value); }
 	}
-
-	private void SaveButton_Clicked(object sender, EventArgs e)
-	{
-    	// Save the file.
-    	File.WriteAllText(_fileName, TextEditor.Text);
-	}
-
-	private void DeleteButton_Clicked(object sender, EventArgs e)
-	{
-    	// Delete the file.
-   		if (File.Exists(_fileName))
-      	  File.Delete(_fileName);
-
-   	 	TextEditor.Text = string.Empty;
-	}
-
-
 
 }
